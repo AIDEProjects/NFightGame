de912cc94c26743d83aca79f3047b9c11fd2ed78
shikeik <2667097598@qq.com>
2025-08-26 20:55:02 +0800

    将绘制坐标转换与翻转等处理封装为方法并将draw封装到role自身, 取消了渲染器

--- core/src/main/java/com/goldsprite/nfightgame/core/TexRole.java
+++ core/src/main/java/com/goldsprite/nfightgame/core/TexRole.java
@@ -2,6 +2,7 @@ package com.goldsprite.nfightgame.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.goldsprite.utils.math.Vector2;
 import com.goldsprite.utils.math.Vector2Int;
@@ -10,15 +11,63 @@ public class TexRole {
 	public Texture roleTex;
 	public TextureRegion region;
 	public Vector2Int texSplit;
-	public Vector2 pivotPos = new Vector2(200, 250);//锚点位置
+	public Vector2 pos = new Vector2(200, 250);//位置
 	public Vector2Int face = new Vector2Int(1, 1);//人物朝向(-1左下1右上
 	public Vector2 roleBound = new Vector2();//角色原始大小
 	public float roleScl = 2f;//角色缩放
-	Vector2Int footOffset = new Vector2Int(119, 36);
+	Vector2Int originOffset = new Vector2Int(119, 36);
+	Vector2Int atkOffset = new Vector2Int(53, 60);
 
 	public TexRole() {
+	}
+
+	public void initTex(){
 		roleTex = new Texture(Gdx.files.internal("hero/hero_sheet.png"));
 		texSplit = new Vector2Int(256, 256);
 		region = new TextureRegion(roleTex, 0, 0, texSplit.x, texSplit.y);
+	}
+
+	public TextureRegion getRegion(){
+		return region;
+	}
+
+	public void act(float delta){
+		roleBound.set(getRegion().getRegionWidth(), getRegion().getRegionHeight());
+		flipRole();
+	}
+
+	public void flipRole(){
+		//翻转人物
+		boolean flipX, flipY;
+		if(face.x >= 0){
+			flipX = getRegion().isFlipX();
+		}else
+			flipX = !getRegion().isFlipX();
+		if(face.y >= 0){
+			flipY = getRegion().isFlipY();
+		}else
+			flipY = !getRegion().isFlipY();
+		getRegion().flip(flipX, flipY);
+	}
+
+	Vector2 leftDownPos = new Vector2();
+	Vector2 sclSize = new Vector2();
+	public Vector2 flipOriginOffset = new Vector2();//脚底实际偏移
+	public void draw(SpriteBatch batch) {
+		//绘制图像
+		batch.draw(region, getLeftDownPos().x, getLeftDownPos().y, getSclSize().x, getSclSize().y);
+	}
+
+	private Vector2 getSclSize() {
+		return sclSize.set(roleBound.x * roleScl, roleBound.y * roleScl);
+	}
+
+	private Vector2 getLeftDownPos() {
+		//计算翻转实际偏移
+		flipOriginOffset.set(originOffset.x, originOffset.y);
+		if(region.isFlipX()) flipOriginOffset.x = roleBound.x-originOffset.x;
+		if(region.isFlipY()) flipOriginOffset.y = roleBound.y-originOffset.y;
+		//返回左下位置
+		return leftDownPos.set(pos.x - flipOriginOffset.x * roleScl, pos.y - flipOriginOffset.y * roleScl);
 	}
 }
--- core/src/main/java/com/goldsprite/nfightgame/examples/RoleRendererExamples.java
+++ core/src/main/java/com/goldsprite/nfightgame/examples/RoleRendererExamples.java
@@ -9,16 +9,15 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.goldsprite.gdxcore.screens.GScreen;
 import com.goldsprite.nfightgame.core.TexRole;
-import com.goldsprite.nfightgame.core.TexRoleRenderer;
-import com.goldsprite.utils.math.Vector2;
 import com.goldsprite.utils.math.Vector2Int;
+import com.goldsprite.nfightgame.core.*;
 
 public class RoleRendererExamples extends GScreen {
 	private Color clearColor = Color.valueOf("#333333FF");
 	private SpriteBatch batch;
 	private ShapeRenderer shapeRenderer;
 	private TexRole role;
-	private TexRoleRenderer roleRenderer;
+	private AnimRole role2;
 
 	@Override
 	public void create() {
@@ -26,7 +25,23 @@ public class RoleRendererExamples extends GScreen {
 		shapeRenderer = new ShapeRenderer();
 
 		role = new TexRole();
-		roleRenderer = new TexRoleRenderer(batch);
+		role.initTex();
+		role.pos.set(200, 250);
+		role.roleScl = 1.8f;
+
+		role2 = new AnimRole();
+		role2.pos.set(400, 250);
+		role2.roleScl = 1.5f;
+		role2.face.x = -1;
+		String path = "hero/hero_sheet.png";
+		Texture hero_sheet = new Texture(Gdx.files.internal(path));
+		int count = 2;
+		Vector2Int cell = new Vector2Int(256, 256);
+		TextureRegion[] frames = new TextureRegion[count];
+		for (int i = 0; i < count; i++) {
+			frames[i] = new TextureRegion(hero_sheet, i*cell.x, 2*cell.y, cell.x, cell.y);
+		}
+		role2.setFrames(.25f, frames, true);
 	}
 
 	@Override
@@ -35,7 +50,13 @@ public class RoleRendererExamples extends GScreen {
 
 		batch.setProjectionMatrix(getCamera().combined);
 		batch.begin();
-		roleRenderer.drawOnFoot(role);
+
+		role.act(delta);
+		role.draw(batch);
+
+		role2.act(delta);
+		role2.draw(batch);
+
 		batch.end();
 
 		shapeRenderer.setProjectionMatrix(getCamera().combined);
@@ -48,8 +69,9 @@ public class RoleRendererExamples extends GScreen {
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
 		shapeRenderer.setColor(Color.GREEN);
 		//绿色脚底标识
-		shapeRenderer.circle(role.pivotPos.x, role.pivotPos.y, 5 * role.roleScl);
+		shapeRenderer.circle(role.pos.x, role.pos.y, 5 * role.roleScl);
 		shapeRenderer.end();
 	}
 
 }
+
