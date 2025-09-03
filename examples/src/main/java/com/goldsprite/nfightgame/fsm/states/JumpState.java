2012f825f74883dc1f484c4f8f5e81268e315b38
shikeik <2667097598@qq.com>
2025-09-03 15:28:23 +0800

    修复跳跃

--- examples/src/main/java/com/goldsprite/nfightgame/fsm/states/JumpState.java
+++ examples/src/main/java/com/goldsprite/nfightgame/fsm/states/JumpState.java
@@ -5,13 +5,12 @@ import com.goldsprite.nfightgame.fsm.interfaces.IEntityFsm;
 
 public class JumpState<F extends IEntityFsm> extends EntityState<F> {
 	boolean isJumped;
-	boolean isOnWall,lastKeyJump;//用于解决蹭墙连跳
+	boolean isOnWall,isKeyJumpUp;//用于解决蹭墙连跳
 
 	@Override
 	public void enter() {
 		isJumped = false;
-		lastKeyJump = false;
-//			Gdx.app.log("", "enter jump"+(int)(new Random().nextFloat()*100));
+		isKeyJumpUp = false;
 		//动画
 		fsm.getAnim().setCurAnim(StateType.Jump);
 		//跳跃力
@@ -20,31 +19,22 @@ public class JumpState<F extends IEntityFsm> extends EntityState<F> {
 
 	@Override
 	public void exit() {
-//			Gdx.app.log("", "exit jump"+(int)(new Random().nextFloat()*100));
 	}
 
 	@Override
 	public void running(float delta) {
-//			Gdx.app.log("", "isGround: "+fsm.isGround());
+		//是否松手
+		if(!fsm.getKeyJump())
+			isKeyJumpUp = true;
 		//已离开地面或身体有接触点(碰撞)则标记已起跳
 		if (!fsm.isGround() || fsm.getBodyCollider().isCollision()) isJumped = true;
 		//持续更新速度与朝向
 		fsm.move(fsm.getKeyDirX());
 		//蹭墙跳
-		if (!isOnWall && fsm.getBodyCollider().isCollision()) {
-			if(fsm.getKeyJump()){
-				if(!lastKeyJump){
-					fsm.setKeyJump(false);
-					lastKeyJump = true;
-				}else{
-					jumping();
-					isOnWall = true;
-					lastKeyJump = false;
-				}
-			}else{
-				jumping();
-				isOnWall = true;
-			}
+		if (!isOnWall && fsm.getBodyCollider().isCollision() && isKeyJumpUp && fsm.getKeyJump()) {
+			jumping();
+			isOnWall = true;
+			isKeyJumpUp = false;
 		}
 		if (isOnWall && !fsm.getBodyCollider().isCollision()){
 			isOnWall = false;
